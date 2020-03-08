33b074dd1bb7216b9418664350a7db4a540e2098
何旭 <444194401@qq.com>
2020-03-08 16:51:27 +0800

    增加插入性能测试

--- code/HSQL/HSQL.Test/UnitTestPerformance.cs
+++ code/HSQL/HSQL.Test/UnitTestPerformance.cs
@@ -2,6 +2,7 @@ using HSQL.Test.TestDataBaseModel;
 using HSQL.Test.TestHelper;
 using HSQL.Test.TestViewModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,10 +22,12 @@ namespace HSQL.Test
         [TestMethod]
         public void TestInsert()
         {
+            var number = 300000;
+
             var database = new Database(Dialect.MySQL, connnectionString);
             database.Delete<Student>(x => x.Age >= 0);
             var list = new List<Student>();
-            for (var i = 0; i < 100000; i++)
+            for (var i = 0; i < number; i++)
             {
                 list.Add(new Student()
                 {
@@ -42,16 +45,67 @@ namespace HSQL.Test
                 var result = database.Insert<Student>(x);
             });
             stopwatch.Stop();
-            var elapsedMilliseconds = $"插入十万条次共耗时：{stopwatch.ElapsedMilliseconds}毫秒";
+
+
+            var qps = number / (stopwatch.ElapsedMilliseconds / 1000.0);
+            var elapsedMilliseconds = $"QPS 为：{qps}";
+        }
+
+        [TestMethod]
+        public void TestAdoNetInsert()
+        {
+            var number = 300000;
+
+            var database = new Database(Dialect.MySQL, connnectionString);
+            database.Delete<Student>(x => x.Age >= 0);
+            var list = new List<Student>();
+            for (var i = 0; i < number; i++)
+            {
+                list.Add(new Student()
+                {
+                    Id = $"{i}",
+                    Name = "zhangsan",
+                    Age = 18,
+                    SchoolId = "123"
+                });
+            }
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            list.ForEach(x =>
+            {
+                using (var connection = new MySqlConnection(connnectionString))
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        connection.Open();
+                        command.CommandText = "INSERT INTO t_student(id,name,age,school_id,birthday) VALUES(@id,@name,@age,@school_id,@birthday);";
+                        command.Parameters.AddRange(new MySqlParameter[] {
+                            new MySqlParameter("@id",x.Id),
+                            new MySqlParameter("@name",x.Name),
+                            new MySqlParameter("@age",x.Age),
+                            new MySqlParameter("@school_id",x.SchoolId),
+                            new MySqlParameter("@birthday",x.Birthday)
+                        });
+                        command.ExecuteNonQuery();
+                        command.Parameters.Clear();
+                    }
+                }
+            });
+            stopwatch.Stop();
+
+            var qps = number / (stopwatch.ElapsedMilliseconds / 1000.0);
+            var elapsedMilliseconds = $"QPS 为：{qps}";
         }
 
         [TestMethod]
         public void TestBatchInsert()
         {
+            var number = 300000;
+
             var database = new Database(Dialect.MySQL, connnectionString);
             database.Delete<Student>(x => x.Age >= 0);
             var list = new List<Student>();
-            for (var i = 0; i < 100000; i++)
+            for (var i = 0; i < number; i++)
             {
                 list.Add(new Student()
                 {
@@ -66,10 +120,59 @@ namespace HSQL.Test
             stopwatch.Start();
             var result = database.Insert<Student>(list);
             stopwatch.Stop();
-            var elapsedMilliseconds = $"插入十万条次共耗时：{stopwatch.ElapsedMilliseconds}毫秒";
+
+            var qps = number / (stopwatch.ElapsedMilliseconds / 1000.0);
+            var elapsedMilliseconds = $"QPS 为：{qps}";
         }
 
+        [TestMethod]
+        public void TestAdoNetBatchInsert()
+        {
+            var number = 300000;
 
+            var database = new Database(Dialect.MySQL, connnectionString);
+            database.Delete<Student>(x => x.Age >= 0);
+            var list = new List<Student>();
+            for (var i = 0; i < number; i++)
+            {
+                list.Add(new Student()
+                {
+                    Id = $"{i}",
+                    Name = "zhangsan",
+                    Age = 18,
+                    SchoolId = "123"
+                });
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            using (var connection = new MySqlConnection(connnectionString))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    connection.Open();
+                    command.Transaction = connection.BeginTransaction();
+                    list.ForEach(x =>
+                    {
+                        command.CommandText = "INSERT INTO t_student(id,name,age,school_id,birthday) VALUES(@id,@name,@age,@school_id,@birthday);";
+                        command.Parameters.AddRange(new MySqlParameter[] {
+                            new MySqlParameter("@id",x.Id),
+                            new MySqlParameter("@name",x.Name),
+                            new MySqlParameter("@age",x.Age),
+                            new MySqlParameter("@school_id",x.SchoolId),
+                            new MySqlParameter("@birthday",x.Birthday)
+                        });
+                        command.ExecuteNonQuery();
+                        command.Parameters.Clear();
+                    });
+                    command.Transaction.Commit();
+                }
+            }
+            stopwatch.Stop();
+
+            var qps = number / (stopwatch.ElapsedMilliseconds / 1000.0);
+            var elapsedMilliseconds = $"QPS 为：{qps}";
+        }
 
         [TestMethod]
         public void TestQuerySingle()
