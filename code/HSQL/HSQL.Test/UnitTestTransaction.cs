f2d5ee04e62f102e295a21ea549ebecaf4d36d5a
何旭 <444194401@qq.com>
2020-06-26 13:38:44 +0800

    1

--- code/HSQL/HSQL.Test/UnitTestTransaction.cs
+++ code/HSQL/HSQL.Test/UnitTestTransaction.cs
@@ -2,6 +2,8 @@ using HSQL.Test.TestDataBaseModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HSQL.Test
 {
@@ -36,9 +38,45 @@ namespace HSQL.Test
             });
         }
 
-        public void a() {
-            string value = "";
 
+        [TestMethod]
+        public void TestTransactionManyTask()
+        {
+            var database = new Database(Dialect.MySQL, "127.0.0.1", "test", "root", "123456");
+
+            List<Task> list = new List<Task>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var task = new Task(() =>
+                {
+                    database.Transaction(() =>
+                    {
+                        for (var i = 0; i < 100; i++)
+                        {
+                            database.Insert<Student>(new Student()
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                Name = "zhangsan",
+                                Age = 18,
+                                SchoolId = "123"
+                            });
+                        }
+                    });
+                });
+                list.Add(task);
+            }
+
+            list.ForEach(x =>
+            {
+                x.Start();
+            });
+
+            list.ForEach(x =>
+            {
+                if (!x.IsCompleted)
+                    x.Wait();
+            });
         }
+
     }
 }
