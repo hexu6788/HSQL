b6088c1d7a0e12db23a2b4df5f4af3f5d3664ad2
何旭 <444194401@qq.com>
2020-07-23 12:03:27 +0800

    增加NugetTest

---
+++ code/HSQL/HSQL.NugetTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using HSQL.MySQL;
+using System;
+using System.Linq;
+
+namespace HSQL.NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbContext dbContext = new DbContext("127.0.0.1", "test", "root", "123456");
+            var list = dbContext.Query("SELECT * FROM t_student;");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
